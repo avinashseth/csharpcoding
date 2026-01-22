4655c2b4c06498499e0f9ef97cbf1415b9a4b51e
Avinash Seth <bloggerbeg@gmail.com>
2026-01-22 11:55:28 +0530

    Create sql_table.sql

---
+++ sql_table.sql
@@ -0,0 +1,6 @@
+create table students (
+    id int not null primary key,
+    student_name nvarchar(100) not null,
+    student_email nvarchar(255) null,
+    student_mobile varchar(15) not null
+);
