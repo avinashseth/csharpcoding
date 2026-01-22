7c728e75b3756a1d98c6e618b93e84842e1d56fb
Avinash Seth <bloggerbeg@gmail.com>
2026-01-22 12:10:24 +0530

    Update sql_table.sql

--- sql_table.sql
+++ sql_table.sql
@@ -9,3 +9,16 @@ create table students (
 insert into students (id, student_name, student_mobile) values (1,'Avinash Seth', '9876543210');
 go
 /* insert data */
+**************************************************
+select * from students;
+go
+**************************************************
+select * from students where id = 1;
+go
+**************************************************
+update students set student_email = 'avinash@google.com' where id = 1;
+go
+**************************************************
+delete from students where id = 2;
+go
+**************************************************
