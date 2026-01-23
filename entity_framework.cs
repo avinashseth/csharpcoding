82bcb8663b324757bc1a9522803bc1c7bb72f268
Avinash Seth <bloggerbeg@gmail.com>
2026-01-23 11:36:24 +0530

    Update entity_framework.cs

--- entity_framework.cs
+++ entity_framework.cs
@@ -6,3 +6,12 @@ dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 10.0.2
 *************************************
 dotnet add package Microsoft.EntityFrameworkCore.Design --version 10.0.2
 *************************************
+Run the following commands
+dotnet restore
+dotnet build
+*************************************
+Run the migration
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+*************************************
+
