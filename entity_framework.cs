de2257396322b6e590180857e63c6f76ad9c37a5
Avinash Seth <bloggerbeg@gmail.com>
2026-01-23 10:01:40 +0530

    Create entity_framework.cs

---
+++ entity_framework.cs
@@ -0,0 +1,8 @@
+dotnet tool install --global dotnet-ef
+*************************************
+dotnet add package Microsoft.EntityFrameworkCore --version 10.0.2
+*************************************
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 10.0.2
+*************************************
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 10.0.2
+*************************************
